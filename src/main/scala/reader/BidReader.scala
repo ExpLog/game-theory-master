1f50fcab1144ad4e97183e45d0d08f5447646642
Leo <lobo.fontoura@gmail.com>
2014-06-19 01:23:07 -0300

    Tries to ignore random line breaks in Bid Reader. Still behaves oddly when more than 500 bids are requested.

--- src/main/scala/reader/BidReader.scala
+++ src/main/scala/reader/BidReader.scala
@@ -14,11 +14,11 @@ object BidReader {
    */
   def readBids(name: String, str: String): List[ImmutableBid] = {
     val lines: List[String] = str.split("\n").toList.drop(1)
-    val bids: List[ImmutableBid] = lines.map{
-      l => l match {
-        case ext"$source $sink $priceBid" =>
-          new ImmutableBid(name, source.toInt, sink.toInt, priceBid.toDouble)
-      }
+    val bids: List[ImmutableBid] = lines.filter{
+      case ext"$source $sink $priceBid" => true
+    }.map{
+      case ext"$source $sink $priceBid" =>
+        new ImmutableBid(name, source.toInt, sink.toInt, priceBid.toDouble)
     }
     bids
   }
